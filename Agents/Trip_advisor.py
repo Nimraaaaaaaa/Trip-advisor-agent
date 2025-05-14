4183760583aa44b49b7b68a7aca16dde3ac9fa73
waris hayat <warishayat666@gmail.com>
2025-05-15 00:49:49 +0500

    model-setup

--- Agents/Trip_advisor.py
+++ Agents/Trip_advisor.py
@@ -1,1 +1,17 @@
-print("Here will write the logic of trip advisor agents")
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+Gemini_api_key =  os.environ.get("GEMINI_API_KEY")
+
+print("Gemini api is set into enviroment.")
+model = ChatGoogleGenerativeAI(
+    model = "gemini-2.0-flash",
+    temperature=0.7,
+    api_key=Gemini_api_key
+)
+
+response=model.invoke("Who is imran khan?").content
+print(response)
